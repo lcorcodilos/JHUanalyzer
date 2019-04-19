d12da926d5ad5d6af07b1e6fef2a1cd34072d4ec
Lucas Corcodilos <lac352@scarletmail.rutgers.edu>
2019-04-18 21:17:38 -0400

    Changed skim location

--- Presel_Functions.py
+++ Presel_Functions.py
@@ -82,7 +82,7 @@ def LoadCuts(region,year):
 #This needs to be updated whenever new Ntuples are produced (unless the file locations are the same).
 def Load_jetNano(string,year):
     print 'running on ' + string 
-    return 'root://cmseos.fnal.gov//store/user/lcorcodi/bstar_nano/rootfiles/'+string+'_hh'+year+'.root'
+    return 'root://cmseos.fnal.gov//store/user/lcorcodi/hhto4b_nano/rootfiles/'+string+'_hh'+year+'.root'
 
 
 def PDF_Lookup(pdfs , pdfOP ):
