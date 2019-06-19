a62825d3de6b73d77c7f9b7a0a44da3f1934e4ad
lcorcodilos <lac352@scarletmail.rutgers.edu>
2019-06-19 11:37:38 -0400

    Update make_lists to have latest produced Ntuples with DDB

--- hhTrees/NanoAOD18_lists/make_lists.py
+++ hhTrees/NanoAOD18_lists/make_lists.py
@@ -4,52 +4,29 @@ import subprocess
 
 # Just grabbing the basic centrally produced nanoAOD so this will be different from the 2016 version
 input_subs = {
-    "ttbar":"/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "ttbar-semilep":"/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "QCDHT700":"/QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAOD-102X_upgrade2018_realistic_v15-v1/NANOAODSIM",
-    "QCDHT2000":"/QCD_HT2000toInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAOD-102X_upgrade2018_realistic_v15-v1/NANOAODSIM",
-    "QCDHT1500":"/QCD_HT1500to2000_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAOD-102X_upgrade2018_realistic_v15-v1/NANOAODSIM",
-    "QCDHT1000":"/QCD_HT1000to1500_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAOD-102X_upgrade2018_realistic_v15-v1/NANOAODSIM",
-    "dataA":"/JetHT/Run2018A-Nano14Dec2018-v1/NANOAOD",
-    "dataB":"/JetHT/Run2018B-Nano14Dec2018-v1/NANOAOD",
-    "dataC":"/JetHT/Run2018C-Nano14Dec2018-v1/NANOAOD",
-    "dataD":"/JetHT/Run2018D-Nano14Dec2018_ver2-v1/NANOAOD",
+    "ttbar":"/store/user/lcorcodi/NanoAOD102X_private/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/*/*/*.root",
+    # "ttbar-semilep":"/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
+    # "QCDHT700":"/QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAOD-102X_upgrade2018_realistic_v15-v1/NANOAODSIM",
+    # "QCDHT2000":"/QCD_HT2000toInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAOD-102X_upgrade2018_realistic_v15-v1/NANOAODSIM",
+    # "QCDHT1500":"/QCD_HT1500to2000_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAOD-102X_upgrade2018_realistic_v15-v1/NANOAODSIM",
+    # "QCDHT1000":"/QCD_HT1000to1500_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAOD-102X_upgrade2018_realistic_v15-v1/NANOAODSIM",
+    "dataA":"/JetHT/Run2018A-Nano1June2019-v2/NANOAOD",
+    "dataB":"/JetHT/Run2018B-Nano1June2019-v2/NANOAOD ",
+    "dataC1":"/JetHT/Run2018C-Nano1June2019-v1/NANOAOD",
+    "dataC2":"/JetHT/Run2018C-Nano1June2019-v2/NANOAOD",
+    "dataD":"/JetHT/Run2018D-Nano1June2019_ver2-v1/NANOAOD",
 
-    "RadNar-1000":"/RadionTohhTohbbhbb_narrow_M-1000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    # "RadNar-1200":"/RadionTohhTohbbhbb_narrow_M-1200_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadNar-1500":"/RadionTohhTohbbhbb_narrow_M-1500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadNar-2000":"/RadionTohhTohbbhbb_narrow_M-2000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadNar-2500":"/RadionTohhTohbbhbb_narrow_M-2500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadNar-3000":"/RadionTohhTohbbhbb_narrow_M-3000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
+    "GravNar-1000":"/store/user/lcorcodi/NanoAOD102X_private/BulkGravTohhTohbbhbb_narrow_M-1000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/*/0000/*.root",
+    "GravNar-1500":"/store/user/lcorcodi/NanoAOD102X_private/BulkGravTohhTohbbhbb_narrow_M-1500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/*/0000/*.root",
+    "GravNar-2000":"/store/user/lcorcodi/NanoAOD102X_private/BulkGravTohhTohbbhbb_narrow_M-2000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/*/0000/*.root",
+    "GravNar-2500":"/store/user/lcorcodi/NanoAOD102X_private/BulkGravTohhTohbbhbb_narrow_M-2500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/*/0000/*.root",
+    "GravNar-3000":"/store/user/lcorcodi/NanoAOD102X_private/BulkGravTohhTohbbhbb_narrow_M-2500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/*/0000/*.root",
+    "RadNar-1000":"/store/user/lcorcodi/NanoAOD102X_private/RadionTohhTohbbhbb_narrow_M-1000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/*/0000/*.root",
+    "RadNar-1500":"/store/user/lcorcodi/NanoAOD102X_private/RadionTohhTohbbhbb_narrow_M-1500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/*/0000/*.root",
+    "RadNar-2000":"/store/user/lcorcodi/NanoAOD102X_private/RadionTohhTohbbhbb_narrow_M-2000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/*/0000/*.root",
+    "RadNar-2500":"/store/user/lcorcodi/NanoAOD102X_private/RadionTohhTohbbhbb_narrow_M-2500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/*/0000/*.root",
+    "RadNar-3000":"/store/user/lcorcodi/NanoAOD102X_private/RadionTohhTohbbhbb_narrow_M-3000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/*/0000/*.root"
 
-    "RadWid05-1000":"/RadionTohhTohbbhbb_width0p05_M-1000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadWid05-1500":"/RadionTohhTohbbhbb_width0p05_M-1500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadWid05-2000":"/RadionTohhTohbbhbb_width0p05_M-2000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadWid05-2400":"/RadionTohhTohbbhbb_width0p05_M-2400_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadWid05-3000":"/RadionTohhTohbbhbb_width0p05_M-3000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-
-    "RadWid10-1500":"/RadionTohhTohbbhbb_width0p10_M-1500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadWid10-2000":"/RadionTohhTohbbhbb_width0p10_M-2000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadWid10-2400":"/RadionTohhTohbbhbb_width0p10_M-2400_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "RadWid10-3000":"/RadionTohhTohbbhbb_width0p10_M-3000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-
-    "GravNar-1000":"/BulkGravTohhTohbbhbb_narrow_M-1000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravNar-1500":"/BulkGravTohhTohbbhbb_narrow_M-1500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravNar-2000":"/BulkGravTohhTohbbhbb_narrow_M-2000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravNar-2500":"/BulkGravTohhTohbbhbb_narrow_M-2500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravNar-3000":"/BulkGravTohhTohbbhbb_narrow_M-3000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-
-    "GravWid05-1000":"/BulkGravTohhTohbbhbb_width0p05_M-1000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravWid05-1500":"/BulkGravTohhTohbbhbb_width0p05_M-1500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravWid05-2000":"/BulkGravTohhTohbbhbb_width0p05_M-2000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravWid05-2400":"/BulkGravTohhTohbbhbb_width0p05_M-2400_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravWid05-3000":"/BulkGravTohhTohbbhbb_width0p05_M-3000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-
-    "GravWid10-1000":"/BulkGravTohhTohbbhbb_width0p05_M-1000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravWid10-1500":"/BulkGravTohhTohbbhbb_width0p05_M-1500_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravWid10-2000":"/BulkGravTohhTohbbhbb_width0p05_M-2000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravWid10-2400":"/BulkGravTohhTohbbhbb_width0p05_M-2400_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
-    "GravWid10-3000":"/BulkGravTohhTohbbhbb_width0p05_M-3000_TuneCP2_PSweights_13TeV-madgraph_pythia8/RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM",
 
 }
 executables = []
