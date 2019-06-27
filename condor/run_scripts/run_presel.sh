1c001c8044479a216ca5112415cd0116848e13f4
dbrehm <brehm.derek@gmail.com>
2019-06-27 12:32:47 -0400

    Run_file changes that I forgot to push and keep having to fix.

--- condor/run_scripts/run_presel.sh
+++ condor/run_scripts/run_presel.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/10XwithNanoClean.tgz ./
+xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/10XwithNano.tgz ./
 export SCRAM_ARCH=slc6_amd64_gcc700
-scramv1 project CMSSW CMSSW_10_2_0
-tar xzf 10XwithNanoClean.tgz
-rm 10XwithNanoClean.tgz
+scramv1 project CMSSW CMSSW_10_2_13
+tar xzf 10XwithNano.tgz
+rm 10XwithNano.tgz
 
 mkdir tardir; cp tarball.tgz tardir/; cd tardir
 tar xzvf tarball.tgz
-cp -r * ../CMSSW_10_2_0/src/JHUanalyzer/
-cd ../CMSSW_10_2_0/src/JHUanalyzer/
+cp -r * ../CMSSW_10_2_13/src/JHUanalyzer/
+cd ../CMSSW_10_2_13/src/JHUanalyzer/
 eval `scramv1 runtime -sh`
 
 echo make_preselection.py $*
