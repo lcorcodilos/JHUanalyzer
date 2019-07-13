5b753a35d2d7774b4bc4543061798fa8437cfaf8
lcorcodilos <lac352@scarletmail.rutgers.edu>
2019-07-13 12:37:25 -0400

    if statement fix

--- condor/arg_makers/hh_nano_sub.py
+++ condor/arg_makers/hh_nano_sub.py
@@ -22,16 +22,17 @@ outfile = open('../args/hh_nano'+name_string+'_args.txt','w')
 
 base_string = '-s TEMPSET -j IJOB -n NJOB -y TEMPYEAR'#'python CondorHelper.py -r run_bstar.sh -a "-s TEMPSET -j IJOB -n NJOB -y TEMPYEAR"'
 
-for year in ['16','17','18'] and year not in ignore:
-    year_string = base_string.replace("TEMPYEAR",year)
+for year in ['16','17','18'] 
+    if year not in ignore:
+        year_string = base_string.replace("TEMPYEAR",year)
 
-    for file in glob.glob('../../hhTrees/NanoAOD'+year+'_lists/*_loc.txt'):
-        setname = file.split('/')[-1].split('_loc')[0]
-        if setname not in ignore:
-            set_njobs = len(open(file,'r').readlines())
-            job_base_string = year_string.replace('TEMPSET',setname).replace('NJOB',str(set_njobs))
-            for i in range(1,set_njobs+1):
-                job_string = job_base_string.replace('IJOB',str(i))
-                outfile.write(job_string+'\n')
+        for file in glob.glob('../../hhTrees/NanoAOD'+year+'_lists/*_loc.txt'):
+            setname = file.split('/')[-1].split('_loc')[0]
+            if setname not in ignore:
+                set_njobs = len(open(file,'r').readlines())
+                job_base_string = year_string.replace('TEMPSET',setname).replace('NJOB',str(set_njobs))
+                for i in range(1,set_njobs+1):
+                    job_string = job_base_string.replace('IJOB',str(i))
+                    outfile.write(job_string+'\n')
 
 outfile.close()
