cd3ccad4a899b70b88c473120372aa64de2ba132
lcorcodilos <lac352@scarletmail.rutgers.edu>
2019-08-14 11:50:57 -0400

    Run script fix

--- condor/run_scripts/run_analyzer.sh
+++ condor/run_scripts/run_analyzer.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 echo "Run script starting"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/10_6_2.tgz ./
 export SCRAM_ARCH=slc7_amd64_gcc700
-scramv1 project CMSSW CMSSW_10_6_2
-tar -xzf 10_6_2.tgz
-rm 10_6_2.tgz
+eval `scramv1 project CMSSW CMSSW_10_6_2`
 
 mkdir tardir; cp tarball.tgz tardir/; cd tardir/
 tar -xzf tarball.tgz; rm tarball.tgz
-cp -r * CMSSW_10_6_2/src/; cd ../CMSSW_10_6_2/src/
-git clone git@github.com:lcorcodilos/JHUanalyzer.git
-cd JHUanalyzer; git checkout rdfDev; cd ../
+cp -r * ../CMSSW_10_6_2/src/; cd ../CMSSW_10_6_2/src/
+eval `scramv1 runtime -sh`
+
+#git clone git@github.com:lcorcodilos/JHUanalyzer.git
+#cd JHUanalyzer; git checkout rdfDev; cd ../
 eval `scramv1 runtime -sh`
 
 echo hh16_preselection.py $*
