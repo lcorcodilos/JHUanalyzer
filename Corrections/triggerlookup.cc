c99c57087e2411a010c83412373a2c28975a812a
dbrehm <brehm.derek@gmail.com>
2019-10-08 22:59:21 -0400

    triggerlookup converted to c++ and pushed for testing.

---
+++ Corrections/triggerlookup.cc
@@ -0,0 +1,36 @@
+#include <cmath>
+using namespace ROOT::VecOps;
+using rvec_f = const RVec<float> &;
+using rvec_i = const RVec<int> &;
+
+namespace analyzer {
+    std::vector<float> Trigger_Lookup(rvec_f var, TH1F* TRP ){
+        float Weight = 1.0;
+        float Weightup = 1.0;
+        float Weightdown = 1.0;
+
+        std::vector<float> out;
+
+        if (var < 2000.0){
+            int bin0 = TRP->FindBin(H); 
+            float jetTriggerWeight = TRP->GetBinContent(bin0);
+            // Check that we're not in an empty bin in the fully efficient region
+            if (jetTriggerWeight == 0){
+                if ((TRP->GetBinContent(bin0-1) == 1.0) && (TRP->GetBinContent(bin0+1) == 1.0)){
+                    jetTriggerWeight = 1.0;
+                }elif (((TRP->GetBinContent(bin0-1) > 0) || (TRP->GetBinContent(bin0+1) > 0))){
+                    jetTriggerWeight = (TRP->GetBinContent(bin0-1)+TRP->GetBinContent(bin0+1))/2.0;
+                }
+
+            Weight = jetTriggerWeight;
+            deltaTriggerEff  = 0.5*(1.0-jetTriggerWeight);
+            Weightup  =   min(1.0,jetTriggerWeight + deltaTriggerEff);
+            Weightdown  =   max(0.0,jetTriggerWeight - deltaTriggerEff);
+            }
+        }    
+        out.push_back(Weight);
+        out.push_back(Weightup);
+        out.push_back(Weightdown);
+        return out;
+    }
+}
