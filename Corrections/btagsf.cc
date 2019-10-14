f59f0d1350cdd22da9c3cbb7ece4739c355a6449
dbrehm <brehm.derek@gmail.com>
2019-10-13 21:39:47 -0400

    more bug fixes for btagsf.cc related to formatting

--- Corrections/btagsf.cc
+++ Corrections/btagsf.cc
@@ -10,15 +10,15 @@
 using namespace ROOT::VecOps;
 
 namespace analyzer {
-  std::vector<float> btagSF(string year, TLorentzVector* b_jet0,TLorentzVector* b_jet1) {
-    	std::vector<float> v;
-	    if (year == "16"){
-            BTagCalibration calib("DeepCSV", "SFs/DeepCSV_2016LegacySF_V1.csv");
-        }else if (year == "17"){
-            BTagCalibration calib("DeepCSV", 'SFs/DeepCSV_94XSF_V4_B_F.csv');
-        }else if (year ==  "16"){
-            BTagCalibration calib("DeepCSV", "SFs/DeepCSV_102XSF_V1.csv");
-        }
+  std::vector<float> BtagSF(string year, TLorentzVector* b_jet0,TLorentzVector* b_jet1) {
+  std::vector<float> v;
+    if (year == "16"){
+          BTagCalibration calib("DeepCSV", "SFs/DeepCSV_2016LegacySF_V1.csv");
+      }else if (year == "17"){
+          BTagCalibration calib("DeepCSV", 'SFs/DeepCSV_94XSF_V4_B_F.csv');
+      }else if (year ==  "16"){
+          BTagCalibration calib("DeepCSV", "SFs/DeepCSV_102XSF_V1.csv");
+      }
 
 		BTagCalibrationReader reader(BTagEntry::OP_LOOSE,  // operating point
                              "central",             // central sys type
@@ -29,20 +29,20 @@ namespace analyzer {
             "incl");               // measurement type
 
 
-      // Note: this is for b jets, for c jets (light jets) use FLAV_C (FLAV_UDSG)
-      double jet_scalefactor = reader.eval_auto_bounds("central", BTagEntry::FLAV_B, b_jet0->Eta(),b_jet0->Pt()); 
-      double jet_scalefactor_up = reader.eval_auto_bounds("up", BTagEntry::FLAV_B, b_jet0->Eta(), b_jet0->Pt());
-      double jet_scalefactor_do = reader.eval_auto_bounds("down", BTagEntry::FLAV_B, b_jet0->Eta(), b_jet0->Pt());
+    // Note: this is for b jets, for c jets (light jets) use FLAV_C (FLAV_UDSG)
+    double jet_scalefactor = reader.eval_auto_bounds("central", BTagEntry::FLAV_B, b_jet0->Eta(),b_jet0->Pt()); 
+    double jet_scalefactor_up = reader.eval_auto_bounds("up", BTagEntry::FLAV_B, b_jet0->Eta(), b_jet0->Pt());
+    double jet_scalefactor_do = reader.eval_auto_bounds("down", BTagEntry::FLAV_B, b_jet0->Eta(), b_jet0->Pt());
 
-      jet_scalefactor *= reader.eval_auto_bounds("central", BTagEntry::FLAV_B, b_jet1->Eta(),b_jet1->Pt()); 
-      jet_scalefactor_up *= reader.eval_auto_bounds("up", BTagEntry::FLAV_B, b_jet1->Eta(), b_jet1->Pt());
-      jet_scalefactor_do *= reader.eval_auto_bounds("down", BTagEntry::FLAV_B, b_jet1->Eta(), b_jet1->Pt());
+    jet_scalefactor *= reader.eval_auto_bounds("central", BTagEntry::FLAV_B, b_jet1->Eta(),b_jet1->Pt()); 
+    jet_scalefactor_up *= reader.eval_auto_bounds("up", BTagEntry::FLAV_B, b_jet1->Eta(), b_jet1->Pt());
+    jet_scalefactor_do *= reader.eval_auto_bounds("down", BTagEntry::FLAV_B, b_jet1->Eta(), b_jet1->Pt());
 
 
-      v.push_back(jet_scalefactor);
-      v.push_back(jet_scalefactor_up);
-      v.push_back(jet_scalefactor_do);
+    v.push_back(jet_scalefactor);
+    v.push_back(jet_scalefactor_up);
+    v.push_back(jet_scalefactor_do);
 
-      return v;
-    }
+    return v;
+  }
 }
