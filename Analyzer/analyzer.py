5294f0dc0157ba7dfbed8f5e84b12bc7befa3031
dbrehm <brehm.derek@gmail.com>
2019-11-04 13:30:58 -0500

    checking type of cloned histogram

--- Analyzer/analyzer.py
+++ Analyzer/analyzer.py
@@ -124,6 +124,7 @@ class analyzer(object):
         self.SetCFunc('''using namespace ROOT::VecOps;
                 double getWeight(int nvtx)
                 {
+                    std::cout << typeid(puWeight).Name() << std::endl;
                     double weight = 1;
                     weight *= puWeight->GetBinContent(puWeight->FindBin(nvtx));
                     return weight;
