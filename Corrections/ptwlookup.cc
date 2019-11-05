0ff326ee654385f2572cf364054c01253176e28f
Derek Brehm <dbrehm@cern.ch>
2019-11-05 11:15:18 -0600

    hopefully the final version of ptwlookup.cc

--- Corrections/ptwlookup.cc
+++ Corrections/ptwlookup.cc
@@ -2,38 +2,39 @@
 #include <stdbool.h>
 using namespace ROOT::VecOps;
 using rvec_i = const RVec<int>;
+using rvec_d = const RVec<double>;
 
 namespace analyzer {
-    std::vector<std::pair<float, bool>> PTWLookup(int nGenJet, rvec_i GPpdgId, int GPstatusFlags, double GPpt, double GPeta, double GPphi, double GPmass,TLorentzVector* jet0, TLorentzVector* jet1 ){
+    std::vector<float> PTWLookup(int nGenJet, rvec_i GPpdgId, rvec_i GPstatusFlags, rvec_d GPpt, rvec_d GPeta, rvec_d GPphi, rvec_d GPmass, TLorentzVector* jet0, TLorentzVector* jet1 ){
 
-        std::vector<std::pair<float, bool>> out;
+        std::vector<float> out;
 
         float genTpt = 0;
         float genTBpt = 0;
         float wTPt, wTbarPt; 
-        bool pair_exists = false;
+        bool pair_exists = 0.0;
 
         // For all gen particles
         for (int i =0; i < nGenJet; i++){
-            if ((GPpdgId == -6) && (GPstatusFlags & (1 << 13))){ 
+            if ((GPpdgId[i] == -6) && (GPstatusFlags[i] & (1 << 13))){ 
                 TLorentzVector* antitop_lv = new TLorentzVector();
-                antitop_lv->SetPtEtaPhiM(GPpt,GPeta,GPphi,GPmass);
+                antitop_lv->SetPtEtaPhiM(GPpt[i],GPeta[i],GPphi[i],GPmass[i]);
                 if ((antitop_lv->DeltaR(*jet0) <0.8) || (antitop_lv->DeltaR(*jet1) <0.8)){
-                    genTBpt = GPpt;
+                    genTBpt = GPpt[i];
                 }
-            }else if ((GPpdgId == 6) && (GPstatusFlags & (1 << 13))){ 
+            }else if ((GPpdgId[i] == 6) && (GPstatusFlags[i] & (1 << 13))){ 
                 TLorentzVector* top_lv = new TLorentzVector();
-                top_lv->SetPtEtaPhiM(GPpt,GPeta,GPphi,GPmass);
+                top_lv->SetPtEtaPhiM(GPpt[i],GPeta[i],GPphi[i],GPmass[i]);
                 if ((top_lv->DeltaR(*jet0) <0.8) || (top_lv->DeltaR(*jet1) <0.8)){
-                    genTpt = GPpt;
+                    genTpt = GPpt[i];
                 }
             }
         }
 
         if ((genTpt == 0) || (genTBpt == 0)){
-            pair_exists = false;
+            pair_exists = 0.0;
         }else{ 
-            pair_exists = true;
+            pair_exists = 1.0;
         }
         
         if (genTpt == 0){ 
@@ -48,7 +49,8 @@ namespace analyzer {
             wTbarPt = exp(0.0615-0.0005*genTBpt);
         }
 
-        out.push_back(std::make_pair(sqrt(wTPt*wTbarPt),pair_exists));
+        out.push_back(sqrt(wTPt*wTbarPt));
+	out.push_back(pair_exists);
         return out;
     }
 }
