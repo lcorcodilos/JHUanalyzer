c17ff12bdb8a46ff7191eddcc671427327a06399
dbrehm <brehm.derek@gmail.com>
2019-11-05 11:22:59 -0500

    possible bug fix for ptwlookup.cc

--- Corrections/ptwlookup.cc
+++ Corrections/ptwlookup.cc
@@ -5,7 +5,7 @@ using rvec_f = const RVec<float> &;
 using rvec_i = const RVec<int> &;
 
 namespace analyzer {
-    std::vector<std::pair<float, bool>> PTWLookup(int nGenJet, rvec_i GPpdgId, int GPstatusFlags, double GPpt, double GPeta, double GPphi, double GPmass,TLorentzVector* jet0, TLorentzVector* jet1 ){
+    std::vector<std::pair<float, bool>> PTWLookup(int nGenJet, rvec_i* GPpdgId, int GPstatusFlags, double GPpt, double GPeta, double GPphi, double GPmass,TLorentzVector* jet0, TLorentzVector* jet1 ){
 
         std::vector<std::pair<float, bool>> out;
 
@@ -16,13 +16,13 @@ namespace analyzer {
 
         // For all gen particles
         for (int i =0; i < nGenJet; i++){
-            if ((GPpdgId == -6) && (GPstatusFlags & (1 << 13))){ 
+            if ((*GPpdgId == -6) && (GPstatusFlags & (1 << 13))){ 
                 TLorentzVector* antitop_lv = new TLorentzVector();
                 antitop_lv->SetPtEtaPhiM(GPpt,GPeta,GPphi,GPmass);
                 if ((antitop_lv->DeltaR(*jet0) <0.8) || (antitop_lv->DeltaR(*jet1) <0.8)){
                     genTBpt = GPpt;
                 }
-            }else if ((GPpdgId == 6) && (GPstatusFlags & (1 << 13))){ 
+            }else if ((*GPpdgId == 6) && (GPstatusFlags & (1 << 13))){ 
                 TLorentzVector* top_lv = new TLorentzVector();
                 top_lv->SetPtEtaPhiM(GPpt,GPeta,GPphi,GPmass);
                 if ((top_lv->DeltaR(*jet0) <0.8) || (top_lv->DeltaR(*jet1) <0.8)){
