44c9144cf0c50cf204504a405cf3cd5cd46e00ea
dbrehm <brehm.derek@gmail.com>
2019-11-21 08:45:15 -0500

    adding more debugging.

--- Corrections/hemispherize.cc
+++ Corrections/hemispherize.cc
@@ -96,6 +96,10 @@ namespace analyzer {
                 cout << i << " fat jet lorentz vector made" << endl;
                 for (unsigned int j =0; j < passing_pair_indices[0].size(); j++){
                     cout << "begin making indices with index " << j << endl;
+                    if (passing_pair_indices[0][j] == '\0' || passing_pair_indices[1][j] == '\0'){
+                        cout << "empty vector value found " << endl;
+                        break;
+                    }
                     cout << "checking stuff " << passing_pair_indices[0][j] << " " << passing_pair_indices[1][j] << endl; 
                     if (passing_pair_indices[0][j] > 100 || passing_pair_indices[1][j] > 100){
                         cout << "uncaught high memory usage" << endl;
