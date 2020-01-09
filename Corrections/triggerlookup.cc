0da7b8ceba671f88e7542b22c4aa9af443c4255b
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-01-09 13:47:23 -0500

    Fix Issue #8 - added optional threshold argument to triggerlookup.cc

--- Corrections/triggerlookup.cc
+++ Corrections/triggerlookup.cc
@@ -4,14 +4,14 @@
 using namespace ROOT::VecOps;
 
 namespace analyzer {
-    std::vector<float> TriggerLookup(float var, TH1D* TRP ){
+    std::vector<float> TriggerLookup(float var, TH1D* TRP, float threshold=0){
         float Weight = 1;
         float Weightup = 1;
         float Weightdown = 1;
 
         std::vector<float> out;
 
-        if (var < 2000.0){
+        if (var < threshold){
             int bin0 = TRP->FindBin(var); 
             float jetTriggerWeight = TRP->GetBinContent(bin0);
             // Check that we're not in an empty bin in the fully efficient region
